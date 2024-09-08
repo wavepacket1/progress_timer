07bce0477a367402e65b5ed52ac34eb61db006c6
wavepacket1 <Mr.aizakku3@gmail.com>
2024-09-08 18:51:52 +0900

    singletonでaudioのインスタンス制限をした

---
+++ app/components/TimerController.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TimerController from './TimerController';
+
+describe('TimerController Component',()=> {
+    it('renders TimerController component',()=>{
+        // const wrapper = shallow(<TimerController />);
+        // expect(wrapper).toMatchSnapshot();
+    });
+})
